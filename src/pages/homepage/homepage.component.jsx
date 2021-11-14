aa74957e722df24c8181d096b5b927ed22cc9a9e
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-11-14 13:09:20 +0300

    feat: add shopPage, add collection-preview component, add collection-item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
 
-import Directory from '../../compoments/directory/directory.component';
-
 const HomePage = () => (
-  <div className="homepage">
+  <div className='homepage'>
     <Directory />
   </div>
 );
